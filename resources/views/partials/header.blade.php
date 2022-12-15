c94acc7d9cc5d75e26d364f0ed5d7c28ce06007e
luishs0 <luishs.doc@gmail.com>
2022-12-15 17:49:06 +0100

    header top

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-    Header.
+    {{-- HEADER TOP --}}
+
+    <div class="header-top d-flex justify-content-end">
+        <div class="container header-top-container text-end">
+            <span class="me-4">DC POWER VISA</span>
+            <span>ADITIONAL DC SITES</span>
+        </div>
+    </div>
+
+    {{-- /HEADER TOP --}}
+
+
+
+    {{-- NAVBAR --}}
+
+    {{-- /NAVBAR --}}
 </header>
