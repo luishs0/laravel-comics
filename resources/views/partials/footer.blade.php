b1e20f480e7c088c64106cea2efc3e755f63d878
luishs0 <luishs.doc@gmail.com>
2022-12-15 19:42:00 +0100

    aggiunto footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="container-footer">
+        <div class="container d-flex justify-content-between container-container-footer">
+            <div class="lists-footer d-flex">
+                <div class="list-footer">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                    </ul>
+                </div>
+                <div class="list-footer">
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                    </ul>
+                </div>
+                <div class="list-footer">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                        <li>Characters</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="img-footer">
+                <img class="img-img-footer" src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
