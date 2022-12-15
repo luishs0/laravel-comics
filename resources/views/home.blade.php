bf98ff10cc126b83a090014726df73d1c22da66a
luishs0 <luishs.doc@gmail.com>
2022-12-15 19:13:08 +0100

    main completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- GRID --}}
     <div class="container-grid p-5">
         <div class="container">
             <div class="row">
@@ -18,4 +19,24 @@
             </div>
         </div>
     </div>
+    {{-- /GRID --}}
+
+    {{-- MAIN-BOTTOM --}}
+    <div class="container-main-bottom">
+        <div class="container">
+            <div class="row row-cols-5 p-5">
+                @for ($i = 0; $i < 5; $i++)
+                    <div class="col d-flex align-items-center">
+                        <div class="img-main-bottom me-2">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                        </div>
+                        <div class="text-main-bottom">
+                            Digital Comics
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </div>
+    {{-- /MAIN-BOTTOM --}}
 @endsection
